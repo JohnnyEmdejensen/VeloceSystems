b411a53202d3c264a547a61c56942e60f658fbce
Johnny Emde Jensen <johnny@emde.dk>
2026-06-03 15:33:06 +0200

    Added WPF client.

---
+++ VeloceCRM.Client/Globals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VeloceCRM.Client
+{
+    public class Globals
+    {
+        public Internals.AppShareClass AppShare { get; private set; } = new Internals.AppShareClass();
+        public Internals.DataShareClass DataShare { get; private set; } = new Internals.DataShareClass();
+        public Internals.DialogHelperClass DialogHelper { get; private set; } = new Internals.DialogHelperClass();
+        public Internals.EventHelperClass EventHelper { get; private set; } = new Internals.EventHelperClass();
+        public Internals.ToolHelperClass ToolHelper { get; private set; } = new Internals.ToolHelperClass();
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
---
+++ VeloceCRM.Client/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VeloceCRM.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Internals.FormSettingsClass _settings;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _settings = new Internals.FormSettingsClass(this);
+            ContentRendered += MainWindow_ContentRendered;
+            Closing += MainWindow_Closing;
+            KeyDown += MainWindow_KeyDown;
+            _settings.Load();
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _settings.Save();
+        }
+
+        private void MainWindow_ContentRendered(object? sender, EventArgs e)
+        {
+
+        }
+    }
+}
