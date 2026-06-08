035963afcedfb095158cc4c0e0cefa77edf51b68
Johnny Emde Jensen <johnny@emde.dk>
2026-06-08 16:34:11 +0200

    Added some GUI to the desktop.

---
+++ VeloceCRM.Client/Internals/FormSettingsClass.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace VeloceCRM.Client.Internals
+{
+    public class FormSettingsClass
+    {
+        public delegate void DialogHandler(object sender, EventArgs e);
+        public event DialogHandler? DialogLoaded;
+
+        private string _folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Emdesoft Developments\Veloce\CRM\Settings";
+        private Window _parent;
+        public FormSettingsClass(Window Parent) 
+        {
+            DirectoryInfo dir = new DirectoryInfo(_folder);
+            if (!dir.Exists) dir.Create();
+            _parent = Parent;
+        }
+
+        public void Load()
+        {
+            var file = new FileInfo(_folder + @"\" + _parent.Tag.ToString() + ".json");
+            if (file.Exists)
+            {
+                string content = "";
+                using (StreamReader sr = new StreamReader(file.FullName))
+                {
+                    content = sr.ReadToEnd();
+                    sr.Close();
+                }
+                if (!string.IsNullOrEmpty(content))
+                {
+                    var data = Newtonsoft.Json.JsonConvert.DeserializeObject<FormDataClass>(content);
+                    if (data != null)
+                    {
+                        if (data.IsMaxed)
+                        {
+                            _parent.WindowState = WindowState.Maximized;
+                        }
+                        else
+                        {
+                            _parent.Top = data.Top;
+                            _parent.Left = data.Left;
+                            _parent.Width = data.Width;
+                            _parent.Height = data.Height;
+                        }
+                        DialogLoaded?.Invoke(this, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+        public void Save()
+        {
+            var data = new FormDataClass
+            {
+                Top = _parent.Top,
+                Left = _parent.Left,
+                Width = _parent.Width,
+                Height = _parent.Height,
+                IsMaxed = _parent.WindowState == WindowState.Maximized,
+            };
+            var content = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+            using (StreamWriter sw = new StreamWriter(_folder + @"\" + _parent.Tag.ToString() + ".json"))
+            {
+                sw.Write(content);
+                sw.Flush();
+                sw.Close();
+            }
+        }
+    }
+    public class FormDataClass
+    {
+        public double Top { get; set; }
+        public double Left { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public bool IsMaxed { get; set; }
+    }
+}
---
+++ VeloceCRM.Client/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VeloceCRM.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Internals.FormSettingsClass _settings;
+        private System.Timers.Timer _timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _timer = new System.Timers.Timer();
+            _timer.Interval = 5000;
+            _timer.Elapsed += _timer_Elapsed;
+            _timer.Enabled = true;
+            _settings = new Internals.FormSettingsClass(this);
+            _settings.DialogLoaded += _settings_DialogLoaded;
+            ContentRendered += MainWindow_ContentRendered;
+            Closing += MainWindow_Closing;
+            KeyDown += MainWindow_KeyDown;
+            _settings.Load();
+            lblDate.Content = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+        }
+
+        private void ShowView()
+        {
+            if (cmdViewCompanies.IsChecked.HasValue)
+            {
+                if (cmdViewCompanies.IsChecked.Value)
+                {
+                    pageCompanies.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    pageCompanies.Visibility = Visibility.Collapsed;
+                }
+            }
+            if (cmdViewDashboard.IsChecked.HasValue)
+            {
+                if (cmdViewDashboard.IsChecked.Value)
+                {
+                    pageDashboard.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    pageDashboard.Visibility = Visibility.Collapsed;
+                }
+            }
+            if (cmdViewPersons.IsChecked.HasValue)
+            {
+                if (cmdViewPersons.IsChecked.Value)
+                {
+                    pagePersons.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    pagePersons.Visibility = Visibility.Collapsed;
+                }
+            }
+            if (pageCompanies.Visibility == Visibility.Collapsed && pageDashboard.Visibility == Visibility.Collapsed && pagePersons.Visibility == Visibility.Collapsed)
+            {
+                cmdViewDashboard.IsChecked = true;
+                pageDashboard.Visibility = Visibility.Visible;
+            }
+        }
+        private void _timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            _timer.Stop();
+            Dispatcher.BeginInvoke(new Action(() => {
+                lblDate.Content = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            }));
+            _timer.Start();
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _settings.Save();
+        }
+
+        private void MainWindow_ContentRendered(object? sender, EventArgs e)
+        {
+
+        }
+
+        private void imgMin_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void imgMax_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+            imgRes.Visibility = Visibility.Visible;
+            imgMax.Visibility = Visibility.Collapsed;
+        }
+
+        private void imgRes_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Normal;
+            imgRes.Visibility = Visibility.Collapsed;
+            imgMax.Visibility = Visibility.Visible;
+        }
+
+        private void imgCls_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void txtTitle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                WindowState = WindowState.Maximized;
+                imgMax.Visibility = Visibility.Collapsed;
+                imgRes.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                this.DragMove();
+            }
+        }
+        private void _settings_DialogLoaded(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                imgMax.Visibility = Visibility.Collapsed;
+                imgRes.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void cmdViewDashboard_Click(object sender, RoutedEventArgs e)
+        {
+            ShowView();
+        }
+
+        private void cmdViewCompanies_Click(object sender, RoutedEventArgs e)
+        {
+            ShowView();
+        }
+
+        private void cmdViewPersons_Click(object sender, RoutedEventArgs e)
+        {
+            ShowView();
+        }
+    }
+}
