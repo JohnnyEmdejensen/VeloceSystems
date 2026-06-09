fabeac05f9545f0ba97c94594880cf5447c2c492
Johnny Emde Jensen <johnny@emde.dk>
2026-06-09 07:26:09 +0200

    Added icons and windows auth.

--- VeloceCRM.Client/Internals/AppShare.cs
+++ VeloceCRM.Client/Internals/AppShare.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace VeloceCRM.Client.Internals
 {
@@ -20,11 +21,41 @@ namespace VeloceCRM.Client.Internals
         public bool Authenticated()
         {
             bool result = false;
-            App.DialogHelper.ShowLoginDialog();
-            if (ActiveUser != null)
+            if (WindowsAuthenticated())
             {
-                result = true;
+                if (ActiveUser != null)
+                {
+                    result = true;
+                }
             }
+            else
+            {
+                App.DialogHelper.ShowLoginDialog();
+                if (ActiveUser != null)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        private bool WindowsAuthenticated()
+        {
+            var c = Mouse.OverrideCursor;
+            Mouse.OverrideCursor = Cursors.Wait;
+            bool result = false;
+            var user = Repositories.UserRepository.GetByAccount(Environment.UserDomainName + "/" + Environment.UserName);
+            if (user != null)
+            {
+                var license = Repositories.LicenseRepository.GetByKey(user.LicenseKey);
+                if (license != null)
+                {
+                    ActiveLicense = license;
+                    ActiveUser = user;
+                    result = true;
+                }
+            }
+            Mouse.OverrideCursor = c;
             return result;
         }
     }
